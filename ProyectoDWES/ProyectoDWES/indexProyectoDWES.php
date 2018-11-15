adf5a5b35c863da5d3996c4e663cab490468332a
Christian Muñiz de la Huerga <christian.munhue.1@educa.jcyl.es>
2018-11-15 10:49:58 +0100

    Primer commit

---
+++ ProyectoDWES/ProyectoDWES/indexProyectoDWES.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html>
+<html>
+    <head>
+        <title>Christian Muñiz de la Huerga</title>
+        <link rel="stylesheet" type="text/css" href="webroot/css/estilos.css"/>
+        <style>
+            h1{
+                font: normal 3.4em "fb", "Trebuchet MS", Helvetica, Arial;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Entorno de Explotación de Christian</h1>
+        <p><a href="doc/181011EjerciciosTema1ChristianMuñizdelaHuerga.pdf" target="_blank">Tema 1: Características del Entorno Servidor.</a></p>
+        <p><a href="doc/181011EjerciciosTema2ChristianMuñizdelaHuerga.pdf" target="_blank">Tema 2: Configuración de los Entornos de Desarrollo y Explotación.</a></p>
+        <p><a href="ProyectoTema3/indexProyectoTema3.php">Tema 3: Características de PHP.</a></p>
+        <p><a href="ProyectoTema4/indexProyectoTema4.php">Tema 4: Técnicas de acceso a datos en PHP.</a></p>
+        <p><a href="indexProyectoDWES.php">Tema 5: Desarrollo de aplicaciones web utilizando codigo embebido.</a></p>
+        <p><a href="indexProyectoDWES.php">Tema 6: Programación orientada a objetos en PHP.</a></p>
+        <p><a href="indexProyectoDWES.php">Tema 7: Programación de servicios web.</a></p>
+        <p><a href="indexProyectoDWES.php">Tema 8: Desarrollo de aplicaciones web dinámicas.</a></p>
+        <div class="atras"><a href="../index.html"><img src="ProyectoTema3/webroot/img/atras.jpg"></a></div>
+        <footer>
+            <p><a href="../index.html">Christian Muñiz de la Huerga</a> Copyright 2018</p>
+        </footer>
+    </body>
+</html>
